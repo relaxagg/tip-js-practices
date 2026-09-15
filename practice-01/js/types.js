497c48c4a555786088838bc0cae5fc76b3bce444
Юрченко Георгий <zhorik.yurchenko2@gmail.com>
2026-09-15 14:31:20 +0300

    docs: add report for tasks 1-2

---
+++ practice-01/js/types.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const res1 = "8" + 2; 
+const res2 = "8" - 2;
+const res3 = Number("8") + 2;
+const res4 = "12">"3";
+const res5 = 12 === "12";
+const res6 = Number("");
+const res7 = Number("text");
+const res8 = Boolean("false");
+const res9 = typeof null;
+const res10 = typeof NaN;
+
+console.log(res1,typeof res1)
+console.log(res2,typeof res2)
+console.log(res3,typeof res3)
+console.log(res4,typeof res4)
+console.log(res5,typeof res5)
+console.log(res6,typeof res6)
+console.log(res7,typeof res7)
+console.log(res8,typeof res8)
+console.log(res9,typeof res9)
+console.log(res10,typeof res10)
